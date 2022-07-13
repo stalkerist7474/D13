9a7e0b0aff1c10816086ebf6c5a0512ea6226434
stalkerist7474 <80361659+stalkerist7474@users.noreply.github.com>
2022-07-13 18:13:13 +0300

    1

--- board/ads/admin.py
+++ board/ads/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Post, Response, Ad
+
+
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Response)
+admin.site.register(Ad)
